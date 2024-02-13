35b6084818ffef24d059583cc41742370c70da61
MatteoVarano <matteovarano06@gmail.com>
2024-02-13 18:57:31 +0000

    a

--- JAVA/Equazione.java
+++ JAVA/Equazione.java
@@ -7,27 +7,21 @@ public class Equazione{
             return;
         }
 
-        double a=0;
-        double b=0;
-        double c=0;
-        double[] numeri = new double[args.length];
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = Double.parseDouble(args[2]);
 
-        for(int i = 0; i < args.length; i++){
-
-            numeri[i]= Double.parseDouble(args[i]);
-
-            if(i == 0){
-                a = numeri[i];
-            }
-            if(i == 1){
-                b = numeri[i];
-            }
-            if(i == 2){
-                c = numeri[i];
-            }
+        double delta = b * b - 4 * a * c;
+        if(delta > 0){
+            double x1 = (- b + Math.sqrt(delta)) / (2 * a);
+            double x2 = (- b - Math.sqrt(delta)) / (2 * a);
+            System.out.println("il valore di x1 e':" + x1);
+            System.out.println("il valore di x2 e':" + x2);
+        } else if (delta == 0) {
+            double x = - b / (2 * a);
+            System.out.println("il valore di x e':" + x);
+        }else{
+            System.out.println("equazione impossibile");
         }
-        
-        
-        
     }
 }
